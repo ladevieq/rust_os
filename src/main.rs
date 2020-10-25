bed7fe0e326a959e53b1d25934136f551b4893c7
ladevieq <ladevie.quentin@gmail.com>
2020-10-25 22:05:39 +0100

    Improved test framework
    Print output to the console
    Do not qemu with a window
    Prints the test functions states

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,59 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 
+mod serial;
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_println!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
 
     for test in tests {
-        test();
+        test.run();
     }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion_failed() {
+    assert_eq!(0, 1);
+}
+
+#[test_case]
+fn trivial_assertion_success() {
+    assert_eq!(1, 1);
 }
 
 mod vga_buffer;
 
 use core::panic::PanicInfo;
 
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_print!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+
+    loop {}
+}
+
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
@@ -32,4 +72,24 @@ pub extern "C" fn _start() -> ! {
     test_main();
 
     loop {}
+}
+
+type QemuIsaDevicePortType = u32;
+const QEMU_ISA_DEVICE_PORT: u16 = 0xf4;
+
+// May not collide with QEMU default exit codes
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(QEMU_ISA_DEVICE_PORT);
+        port.write(exit_code as QemuIsaDevicePortType);
+    }
 }

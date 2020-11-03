045048ceec9120e902620ffebeb75fe40e4df59e
ladevieq <ladevie.quentin@gmail.com>
2020-11-03 17:35:42 +0100

    Update work

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,14 @@
 
 pub mod gdt;
 pub mod interrupts;
+pub mod memory;
 pub mod serial;
 pub mod vga_buffer;
 
 use core::panic::PanicInfo;
+
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
 
 pub trait Testable {
     fn run(&self) -> ();
@@ -43,10 +47,12 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     hlt_loop();
 }
 
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
 // Custom entry point
 #[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
     init();
     test_main();
     hlt_loop();
--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,37 @@
 #![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use rust_os::{hlt_loop, println};
 
+entry_point!(kernel_main);
+
 // Custom entry point
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello World{}", '!');
 
     rust_os::init();
 
     x86_64::instructions::interrupts::int3();
+
+    use rust_os::memory::translate_addr;
+    use x86_64::structures::paging::PageTable;
+    use x86_64::VirtAddr;
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let addresses = [
+        0xb8000,
+        0x201008,
+        0x0100_0020_1a10,
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!("{:?} -> {:?}", virt, phys);
+    }
 
     #[cfg(test)]
     test_main();
